0b78117cd160feba4c782f43bc63fc4aa24b4cbb
wachka06 <wachka06@gmail.com>
2018-07-10 18:56:06 +0000

    done

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,28 @@ def first_challenge
   }
 
   #your code here
-
+  
+  # contacts.each do |name|
+  #   if name == "Freddy Mercury"
+  #   name.each do |key, val|
+  #     if key == :favorite_icecream_flavors
+      
+  #         val.delete_if{|flavor| flavor == "strawberry"}
+        
+          
+  #       end
+  #     end
+  #   end
+  # end
+  
+  contacts["Freddy Mercury"].each do |key, val|
+    
+      if key == :favorite_icecream_flavors
+      
+          val.delete_if{|flavor| flavor == "strawberry"}
+        
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,7 @@ def second_challenge
   }
 
   #code your solution here!
-
+  result = groceries.values.flatten
   
 
 end
